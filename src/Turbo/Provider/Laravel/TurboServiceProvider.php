fa9b6253a435de3036d3eabb3e6d9f25b9103584
Rob Crowe <hello@vivalacrowe.com>
2013-04-28 21:55:13 +0100

    Added docblocks

--- src/Turbo/Provider/Laravel/TurboServiceProvider.php
+++ src/Turbo/Provider/Laravel/TurboServiceProvider.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ * Think turbolinks but for your PHP application.
+ *
+ * @author Rob Crowe <hello@vivalacrowe.com>
+ * @license MIT
+ */
+
 namespace Turbo\Provider\Laravel;
 
 use Illuminate\Support\ServiceProvider;
+use App;
 use Turbo\Turbo;
 
+/**
+ * Brings the power of Turbo/PJAX to Laravel.
+ */
 class TurboServiceProvider extends ServiceProvider
 {
     /**
@@ -14,7 +25,7 @@ class TurboServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        \App::after(function($request, $response) {
+        App::after(function($request, $response) {
 
             $turbo = new Turbo($response->getOriginalContent()->render());
 
--- src/Turbo/Turbo.php
+++ src/Turbo/Turbo.php
@@ -1,11 +1,31 @@
 <?php
 
+/**
+ * Think turbolinks but for your PHP application.
+ *
+ * @author Rob Crowe <hello@vivalacrowe.com>
+ * @license MIT
+ */
+
 namespace Turbo;
 
+/**
+ * Returns the body of the HTML along with any title.
+ *
+ * Checks for a PJAX request and tailors the response.
+ */
 class Turbo
 {
+    /**
+     * @var string HTML response.
+     */
     protected $response;
 
+    /**
+     * Create a new instance.
+     *
+     * @param string $response HTML.
+     */
     public function __construct($response)
     {
         $this->response = $response;
@@ -15,16 +35,31 @@ class Turbo
         }
     }
 
+    /**
+     * Get the response (May or may not have been processed).
+     *
+     * @return string
+     */
     public function getResponse()
     {
         return $this->response;
     }
 
+    /**
+     * Is this request being made by PJAX.
+     *
+     * @return bool
+     */
     public function isPjax()
     {
         return (isset($_SERVER['HTTP_X_PJAX']) OR isset($_GET['_pjax']));
     }
 
+    /**
+     * If PJAX request, extract HTML body.
+     *
+     * @return void
+     */
     protected function process()
     {
         // We only process if we find a valid <body>
